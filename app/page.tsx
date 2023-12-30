bf3955f1f0231928cc4a3d8d98546c8f9702136f
Nicholas Okoro <54383553+Nickblaq@users.noreply.github.com>
2023-12-30 10:50:11 +0100

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-
+import Landing from "./landing.tsx"
 
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-     
+     <Landing />
     </main>
   )
 }
